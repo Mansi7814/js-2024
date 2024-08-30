77eaf6f24140816303e16fe653aee89c8e1d6f5a
Mansi7814 <57342292+Mansi7814@users.noreply.github.com>
2024-08-30 17:53:28 +0000

    container- nodejs and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+console.log("Mansi");
+
+let variable1 = "One"
+const variable2 = "Two"
+let variable3 = variable2
+
+//variable2 = "three" -- not allowed
+
+variable3 = "three"
+
+console.log(variable1,variable2, variable3);
+
+console.table([variable1,variable2,variable3])
+
+console.log(variable1.indexOf("e"),variable1.charAt('1'))
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
