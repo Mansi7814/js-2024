3b9e8544b08c1eabe7280e7fbad0d0b740cee975
Mansi7814 <57342292+Mansi7814@users.noreply.github.com>
2024-08-31 10:28:24 +0000

    operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,79 @@
+let name = "Mansi"
+console.log(name);
+console.log(typeof name);
+
+let newVar = Number(name)
+console.log(newVar);
+console.log(typeof newVar);
+
+let num = 33
+console.log(num);
+console.log(typeof num);
+
+let newVar1 = String(num)
+console.log(newVar1);
+console.log(typeof newVar1);
+
+let isLoggedIn = "Mansi Jain"
+console.log(isLoggedIn);
+console.log(typeof isLoggedIn);
+
+let BoolenaCheck = Boolean(isLoggedIn)
+console.log(BoolenaCheck);
+console.log(typeof BoolenaCheck);
+
+let BoolenaCheck1 = Boolean(null)
+console.log(BoolenaCheck1);
+console.log(typeof BoolenaCheck1);
+
+
+// ********************** Note **********************
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+// 1 => true; 0 => false
+// "" => false
+// "Mansi Jain" => true
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
